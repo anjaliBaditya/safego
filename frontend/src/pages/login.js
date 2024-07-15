e93e328b11c6685b5d538adada6d27fd4757758d
anjaliBaditya <anjalibaditya8@gmail.com>
2024-07-15 19:05:31 +0000

    safego ui

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -104,7 +104,110 @@ const Login = () => {
                 overflow: "hidden",
               }}
             >
-             
+              <svg
+                className="w-7 absolute inset-0 mt-5 ml-5"
+                fill="white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                onClick={() => window.history.back()}
+              >
+                <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+              </svg>
+              <Typography
+                variant="h3"
+                component="h2"
+                color="primary.contrastText"
+                sx={{
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  mt: { mobile: 2, tablet: 5, laptop: 5 },
+                  mb: 2,
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  fontSize: "2.5rem",
+                }}
+              >
+                Welcome Back
+              </Typography>
+              <Typography
+                variant="h6"
+                color="primary.contrastText"
+                sx={{
+                  fontStyle: "italic",
+                  letterSpacing: "0.1rem",
+                  textAlign: "center",
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  mb: 8,
+                }}
+              >
+                “Log into your account”
+              </Typography>
+              <Grid container justifyContent="center" spacing={3}>
+                <Grid item mobile={11} tablet={8.5} laptop={6}>
+                  <CssTextField
+                    label="Phone"
+                    variant="outlined"
+                    fullWidth
+                    type={"number"}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </Grid>
+                <Grid item mobile={11} tablet={8.5} laptop={6}>
+                  <CssTextField
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    type={"password"}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+              <Typography
+                variant="h6"
+                color="primary.contrastText"
+                sx={{
+                  fontStyle: "italic",
+                  fontSize: "0.65rem",
+                  textAlign: "center",
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  mt: 5,
+                }}
+              >
+                {" "}
+                By logging in you agree to our terms and conditions
+              </Typography>
+              <div className="flex flex-col justify-center items-center mt-20">
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "custom.main",
+                    color: "custom.contrastText",
+                    width: { mobile: "90%", tablet: "40%", laptop: "40%" },
+                  }}
+                  type="submit"
+                >
+                  Log In
+                </Button>
+                <Typography
+                  variant="h6"
+                  component={Link}
+                  color="primary.contrastText"
+                  to={"/signup"}
+                  sx={{
+                    fontStyle: "italic",
+                    fontSize: "0.65rem",
+                    textAlign: "center",
+                    textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                    mt: 1,
+                  }}
+                >
+                  {" "}
+                  Don't have an account Signup
+                </Typography>
+              </div>
+            </Box>
+          </Grid>
+        </Grid>
+      </Theme>
     </>
   );
 };
